6f8bf92f19455467739b996afe57dd70d7eb6534
Swain SHrestha <swainstha@outlook.com>
2017-12-04 11:14:53 +0545

    Update BinarySearchTree.java

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -35,7 +35,7 @@ public class BinarySearchTree {
 
         if(current == null)
             return;
-
+        // finding the node to be deleted
         while(current != null && current.getData() != data) {
             parent = current;
             if(data < current.getData()) {
@@ -47,6 +47,7 @@ public class BinarySearchTree {
             }
         }
 
+        // if the node to be deleted is a leaf node
         if(current.getLeftChild() == null && current.getRightChild() == null) {
             if (current == null) {
                 return;
@@ -57,32 +58,32 @@ public class BinarySearchTree {
                     parent.setRightChild(null);
                 }
             }
-        } else if(current.getRightChild() == null) {
+        } else if(current.getRightChild() == null) { //if the node to be deleted contains only a left node and that left node is a leaf node
             if(current == root)
                 root = current.getLeftChild();
             else if(isLeftChild)
                 parent.setLeftChild(current.getLeftChild());
             else
                 parent.setRightChild(current.getLeftChild());
-        } else if(current.getLeftChild() == null) {
+        } else if(current.getLeftChild() == null) {     //if the node to be deleted contains only a right node and that right node is a leaf node
             if(current == root)
                 root = current.getRightChild();
             else if(isLeftChild)
                 parent.setLeftChild(current.getRightChild());
             else
                 parent.setRightChild(current.getRightChild());
-        } else {
+        } else {                                        //if the node to be deleted is in the middle of the tree
             TreeNode node = current.getRightChild();
             System.out.println(node);
             TreeNode prevNode = new TreeNode(0);
-            if(node.getLeftChild() == null && node.getRightChild() == null) {
+            if(node.getLeftChild() == null && node.getRightChild() == null) {   //if the right node of the deleted node is a leaf node
                 node.setLeftChild(current.getLeftChild());
                 if(isLeftChild)
                     parent.setLeftChild(node);
                 else
                     parent.setRightChild(node);
             }
-            else if(node.getLeftChild() != null) {
+            else if(node.getLeftChild() != null) { //if the right node of the deleted node is not a leaf node
                 while (node.getLeftChild() != null) {
                     prevNode = node;
                     node = node.getLeftChild();
@@ -106,13 +107,13 @@ public class BinarySearchTree {
                 }
                 if(current == root)
                     root = node;
-            } else if(node.getLeftChild() == null && parent != null){
+            } else if(node.getLeftChild() == null && parent != null){ //if the deleted node is not a root node and the left child of the deleted node is null
                 node.setLeftChild(current.getLeftChild());
                 if(isLeftChild)
                     parent.setLeftChild(node);
                 else
                     parent.setRightChild(node);
-            } else if(node.getLeftChild() == null && parent == null) {
+            } else if(node.getLeftChild() == null && parent == null) { //if the deleted node is a root node
                 node.setLeftChild(current.getLeftChild());
                 root = node;
             }
